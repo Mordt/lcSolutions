21af4730f15a8cb94dcd1b452400da2898521f58
Carlos Quintero <0xcquintero@gmail.com>
2022-03-18 20:11:20 +1100

    added alternate solution for removeElement

--- RemoveElement/removeElement.py
+++ RemoveElement/removeElement.py
@@ -9,3 +9,14 @@ class Solution(object):
             nums.remove(val)
                 
         return len(nums)
+
+    #alternate solution:
+    """
+    count=0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[count] = nums[i]
+            count +=1
+    return count
+
+    """
