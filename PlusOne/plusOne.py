1d7fa92a18b843dfe26fb09ea713ab57f221159b
Carlos Quintero <0xcquintero@gmail.com>
2022-03-19 13:17:48 +1100

    iterating through digits to start

---
+++ PlusOne/plusOne.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for x in digits:
+            print(x)
