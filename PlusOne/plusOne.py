1f00e936d32d37fd486fab4f3d3c698ee098c298
Carlos Quintero <0xcquintero@gmail.com>
2022-03-19 13:27:07 +1100

    first trial run of PlusOne with added flag

--- PlusOne/plusOne.py
+++ PlusOne/plusOne.py
@@ -4,5 +4,19 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        for x in digits:
-            print(x)
+        #use not added flag to signify wheter current digit should be overflown
+        #set to negative when addition has been complete
+        #iterate through reversed digits for ease
+
+        notAdded = True
+
+        for i, x in enumerate(reversed(digits)):
+            if notAdded:
+                if x == 9:
+                    x = 0
+                    continue
+                else:
+                    x += 1
+                    notAdded = False
+
+        return digits
