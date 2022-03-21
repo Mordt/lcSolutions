c418f1c40030c4ad95a59cadc6558ab00ab5610d
Carlos Quintero <0xcquintero@gmail.com>
2022-03-21 14:36:57 +1100

    first attempt at lenlastword

--- LengthOfLastWord/lengthOfLastWord.py
+++ LengthOfLastWord/lengthOfLastWord.py
@@ -6,3 +6,16 @@ class Solution(object):
         """
         #iterate backwards through string
         #count first non whitespace instance, return
+        count = 0
+        inWord = False
+        for x in reversed(s):
+            if x != ' ':
+                inWord = True
+                count += 1
+            else:
+                inWord = False
+
+            if x == ' ' and count != 0:
+                break
+
+        return count
