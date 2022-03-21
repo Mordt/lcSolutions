38ccdbe4b8879c09e2aa3e006790b409d68e31b3
Carlos Quintero <0xcquintero@gmail.com>
2022-03-21 14:28:31 +1100

    added new class

---
+++ LengthOfLastWord/lengthOfLastWord.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        #iterate backwards through string
+        #count first non whitespace instance, return
