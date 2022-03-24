89c0fa8e9a2935ab7362a28b5440ea52f10a9a2f
Carlos Quintero <0xcquintero@gmail.com>
2022-03-24 14:28:45 +1100

    completed helping dad

--- snippets/date_today.php
+++ snippets/date_today.php
@@ -1,7 +1,9 @@
 //helping dad
+//display a date in a website
 
-function wpb_date_today($atts,$content = null){
+function wpb_date_today($atts, $content = null){ 
     extract(shortcode_atts(array('format' => ''), $atts ));
+    
     if($atts['format'] == ''){
         $date_time .= date(get_option('date_format'));
     } else {
@@ -9,4 +11,13 @@ function wpb_date_today($atts,$content = null){
     }
     return $date_time;
 }
+
 add_shortcode('date_today','wpb_date_today');
+
+function dateMonthYear(){
+    $date = date('d-m-Y h:i:s');
+    return $date;
+}
+
+add_shortcode('date_today','dateMonthYear')
+
