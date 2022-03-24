82483cc3fb7388f8c11740c9a41cc2aec2ad150f
Carlos Quintero <0xcquintero@gmail.com>
2022-03-24 14:04:10 +1100

    fixing style

--- snippets/date_today.php
+++ snippets/date_today.php
@@ -1,10 +1,12 @@
+//helping dad
+
 function wpb_date_today($atts,$content = null){
-extract(shortcode_atts(array('format' => ''), $atts ));
-if($atts['format'] == ''){
-$date_time .= date(get_option('date_format'));
-} else {
-$date_time .= date($atts['format']);
-}
-return $date_time;
+    extract(shortcode_atts(array('format' => ''), $atts ));
+    if($atts['format'] == ''){
+        $date_time .= date(get_option('date_format'));
+    } else {
+        $date_time .= date($atts['format']);
+    }
+    return $date_time;
 }
 add_shortcode('date_today','wpb_date_today');
