7707070dc1715e9252861f87da999854215a5a8e
Carlos Quintero <0xcquintero@gmail.com>
2022-03-26 15:30:04 +1100

    third attempt sqrtx

--- SqrtX/mySqrt.py
+++ SqrtX/mySqrt.py
@@ -8,6 +8,11 @@ class Solution(object):
         #cant do this:
             #return int(sqrt(x))
         integers = range(1,x)
+        domain = []
         for num in integers:
-            if num*num == x:
-                return num       
+            if num*num >= x:
+                print num
+                domain = range(1,num)
+                break
+        print(domain)
+        return domain[len(domain)-1]
