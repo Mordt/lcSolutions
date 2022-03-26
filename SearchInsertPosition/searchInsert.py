1dbed3f82db5821a5b7d5ca6e92f277538434b84
Carlos Quintero <0xcquintero@gmail.com>
2022-03-26 14:33:56 +1100

    added program file searchInsert

---
+++ SearchInsertPosition/searchInsert.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        if target in nums:
+            return index(target)
+        else:
+           
