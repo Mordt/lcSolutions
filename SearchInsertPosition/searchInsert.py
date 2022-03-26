6756327299b9acef9ce5d9937df0b297a4cfa40e
Carlos Quintero <0xcquintero@gmail.com>
2022-03-26 14:43:58 +1100

    added comments to search insert for clarity and to outline thought process

--- SearchInsertPosition/searchInsert.py
+++ SearchInsertPosition/searchInsert.py
@@ -6,6 +6,12 @@ class Solution(object):
         :rtype: int
         """
         
+        #first idea was to insert in order,
+        #however this required an additional library, and I didn't
+        #want to do that.
+        #instead, i thought it would be smarter to insert, sort, then
+        #return the index :)
+        
         if target in nums:
             return nums.index(target)
         else:
