77aed4c4725e35ef32b27d55fc6f308b9df55d37
Carlos Quintero <0xcquintero@gmail.com>
2022-03-26 15:20:54 +1100

    first solution for sqrtx, illegal tho

---
+++ SqrtX/mySqrt.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        #what number, between 1 and x times itself is equal to x?
+        #cant do this:
+        return int(sqrt(x))
+        
