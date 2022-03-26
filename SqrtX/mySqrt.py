f24ba4225599e90653a712d9cf8816b1df5580b6
Carlos Quintero <0xcquintero@gmail.com>
2022-03-26 15:34:02 +1100

    fourth attempt

--- SqrtX/mySqrt.py
+++ SqrtX/mySqrt.py
@@ -7,12 +7,13 @@ class Solution(object):
         #what number, between 1 and x times itself is equal to x?
         #cant do this:
             #return int(sqrt(x))
+        #idea instead search small integer space of possible answers and see
+        #if num*num = x
         integers = range(1,x)
         domain = []
         for num in integers:
-            if num*num >= x:
-                print num
+            if num*num > x:
                 domain = range(1,num)
                 break
-        print(domain)
         return domain[len(domain)-1]
+
