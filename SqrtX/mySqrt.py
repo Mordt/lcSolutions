2dda2a6cca9943fcf9841ecf5d4030924ddafcd6
Carlos Quintero <0xcquintero@gmail.com>
2022-03-26 15:21:28 +1100

    second attempt sqrtx

--- SqrtX/mySqrt.py
+++ SqrtX/mySqrt.py
@@ -6,5 +6,8 @@ class Solution(object):
         """
         #what number, between 1 and x times itself is equal to x?
         #cant do this:
-        return int(sqrt(x))
-        
+            #return int(sqrt(x))
+        integers = range(1,x)
+        for num in integers:
+            if num*num == x:
+                return num       
