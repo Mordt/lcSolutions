e467d5889b28204faaacf4be6260970044fcf456
Carlos Quintero <0xcquintero@gmail.com>
2022-03-30 01:25:11 +1100

    solution for next step

--- ClimbingStairs/climbStairs.py
+++ ClimbingStairs/climbStairs.py
@@ -4,4 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        #dynamic programming really
+        #emulates fibonacci sequence
+
+        one, two = 1,1
+
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
