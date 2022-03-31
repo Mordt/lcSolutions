363cf1530a70ab9aa6942129e5e416d307778464
Carlos Quintero <0xcquintero@gmail.com>
2022-03-31 18:57:39 +1100

    latest version merge sorted array

--- MergeSortedArray/merge.py
+++ MergeSortedArray/merge.py
@@ -7,17 +7,14 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
+        print(len(nums1))
+        for i in range(-1,n):
+            print(-i, nums1[-i])
+            if nums1[-i] == 0:
+                nums1.pop(-i)
+        
         for x in nums2:
             nums1.insert(0, x)
-            
-        #while 0 in nums1:
-        #    nums1.remove(0)
-            
-        for i in range(m,m+n):
-            if nums1[i] == 0:
-                nums1.del(i)
                 
         nums1.sort()
-
-
 
