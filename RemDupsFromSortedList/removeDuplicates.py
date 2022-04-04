f82c85c47dd5fe1a42f0ee1e8bd2b46b613b1182
Carlos Quintero <0xcquintero@gmail.com>
2022-04-04 21:56:19 +1000

    first attempt

---
+++ RemDupsFromSortedList/removeDuplicates.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        #have last num
+        #if currnum == lastnum, delete currnum
+
+        while head != None:
+            print head.val
