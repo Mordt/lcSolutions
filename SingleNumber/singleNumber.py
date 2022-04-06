f5232e6c3d1f49fcaa29c1a436da524914dbb208
Carlos Quintero <0xcquintero@gmail.com>
2022-04-06 12:00:38 +1000

    XOR solution singleNumber

--- SingleNumber/singleNumber.py
+++ SingleNumber/singleNumber.py
@@ -4,17 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return
-
-        #new approach, find solo num
-        #check left and right
-        currNum = nums[0]
-        array = len(nums)
-
-        for x in range(1, array):
-            if nums[x] == currNum:
-                continue
-            else:
-                currNum = nums[x]
-                return nums[x]
+        xor = nums[0]
+        for i in range(1, len(nums)):
+            xor ^= nums[i]
+        return xor
