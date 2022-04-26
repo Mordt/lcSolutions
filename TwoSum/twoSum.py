28520b533b95e5e840fd81d88e8ad91b76b3294d
Carlos Quintero <0xcquintero@gmail.com>
2022-04-26 22:16:33 +1000

    thinking about the problem first

--- TwoSum/twoSum.py
+++ TwoSum/twoSum.py
@@ -6,5 +6,16 @@ class Solution(object):
         :rtype: List[int]
         """
 
+        #discussion:
+        """
+        obvious first solution is to brute force the thing, checking each element
+        with every other element to find the solution, returning the indices as required
+        this, however, takes O(n) time in the worst case. not good.
+        
+        what can we do instead? these are the data structures available to us:
+            hashmaps is my first guess because of the complexity of everything
+            perhaps sorting the nums list first might work.
+        """
+
         for x in nums:
             print(x)
