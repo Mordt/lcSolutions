6a04a29f2d49f93f41fa0da9a183be40ffee50ab
Carlos Quintero <0xcquintero@gmail.com>
2022-04-30 16:14:24 +1000

    cleaned up comments twosum

--- TwoSum/twoSum.py
+++ TwoSum/twoSum.py
@@ -5,35 +5,6 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-
-        #discussion:
-        """
-        obvious first solution is to brute force the thing, checking each element
-        with every other element to find the solution, returning the indices as required
-        this, however, takes O(n) time in the worst case. not good.
-        
-        what can we do instead? these are the data structures available to us:
-            hashmaps is my first guess because of the complexity of everything
-            perhaps sorting the nums list first might work.
-            
-        we will start with the brute force solution.
-        
-        toReturn = []
-        for x in nums:
-            for y in nums:
-                #print(nums.index(y), nums.index(x))
-                if nums.index(y) == nums.index(x):
-                    
-                    continue
-                elif target-x == y:  
-                    print("checck")
-                    toReturn.insert(0, nums.index(x))
-                    toReturn.insert(1, nums.index(y))
-                    
-                    return toReturn
-        """
-        #now the optimal solution
-
         prevMap = {}  # val -> index
 
         for i, n in enumerate(nums):
