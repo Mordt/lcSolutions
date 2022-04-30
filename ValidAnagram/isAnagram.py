898ff03e243755e581ede6f736f2818b6e06aceb
Carlos Quintero <0xcquintero@gmail.com>
2022-04-30 17:56:43 +1000

    work on valid anagram

--- ValidAnagram/isAnagram.py
+++ ValidAnagram/isAnagram.py
@@ -5,3 +5,8 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        if s.length() != t.length():
+            return false
+        #use hashmap? trying to compare if every letter in string s exists in t
+
+        for i, x in enumerate s:
