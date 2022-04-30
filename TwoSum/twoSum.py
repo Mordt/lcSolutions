ce6a971dce5e0ca306dfd5f3052a70fa65297eba
Carlos Quintero <0xcquintero@gmail.com>
2022-04-30 16:15:28 +1000

    clarifying comments twosum

--- TwoSum/twoSum.py
+++ TwoSum/twoSum.py
@@ -5,8 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        #assuming step one is  creating the hashmap
         prevMap = {}  # val -> index
 
+        #iterate through nums array, record difference
+        #if in hashmap, return indexes
         for i, n in enumerate(nums):
             diff = target - n
             if diff in prevMap:
