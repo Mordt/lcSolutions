a9059d3e5f948ce2d85390c0b9190def53be94fc
Carlos Quintero <0xcquintero@gmail.com>
2022-04-30 16:21:37 +1000

    twosum, added soln notes

--- TwoSum/twoSum.py
+++ TwoSum/twoSum.py
@@ -21,5 +21,14 @@ class Solution(object):
         notes from video solution:
         type of problem: arrays and hashing
             first instinct is obviously to brute force with a nested loop, O(n^2)
+            for each number, we are looking for the difference between the target and the value
+            for example if target = 4 and value = 1,
+                we are looking for diff = 4-1 = 3
+            can ignore the other numbers, jsut wanna know if 3 exists
 
+            most efficient way is to make a hash map of every value in our input array
+                so we can instantly check if the value 3 exists.
+                
+            in hashmap, map value of array to index.
+            
         """
