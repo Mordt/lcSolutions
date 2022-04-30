cbbd9e7781f45a5e1cf71e54ebc0f95a521fc0bd
Carlos Quintero <0xcquintero@gmail.com>
2022-04-30 16:18:03 +1000

    clarifying comments twosum, added soln notes

--- TwoSum/twoSum.py
+++ TwoSum/twoSum.py
@@ -10,8 +10,16 @@ class Solution(object):
 
         #iterate through nums array, record difference
         #if in hashmap, return indexes
+        #otherwise, map a new value into the hashmap
         for i, n in enumerate(nums):
             diff = target - n
             if diff in prevMap:
                 return [prevMap[diff], i]
             prevMap[n] = i
+
+        """
+        notes from video solution:
+        type of problem: arrays and hashing
+            first instinct is obviously to brute force with a nested loop, O(n^2)
+
+        """
