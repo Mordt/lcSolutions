62721cd0761e20232dddf7c309d5632b2eb509d4
Carlos Quintero <0xcquintero@gmail.com>
2022-05-03 15:44:51 +1000

    isAnagram added Neetcode solution

--- ValidAnagram/isAnagram.py
+++ ValidAnagram/isAnagram.py
@@ -36,3 +36,20 @@ class Solution(object):
             return True
         else:
             return False
+
+        """
+        Neetcode Solution:
+
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
+        """
