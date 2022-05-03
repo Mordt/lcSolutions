2ae702ef15c48cf9ec232ee70cce343e06a8e676
Carlos Quintero <0xcquintero@gmail.com>
2022-05-03 18:15:46 +1000

    implemented hash solution

--- ContainsDuplicate/containsDuplicate.py
+++ ContainsDuplicate/containsDuplicate.py
@@ -4,9 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        """
         data = set()
         for i in nums:
             if i in data:
                 return True
             data.add(i)
         return False
+        """
+        #new solution
+        dupMap = {}  # value -> count
+
+        for x in nums:
+            if x not in dupMap:
+                dupMap[x] = 1
+            else:
+                return True
