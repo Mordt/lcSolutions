bc94fd636af5fd145184cc25006a692032f15a6f
Carlos Quintero <0xcquintero@gmail.com>
2022-05-03 15:08:58 +1000

    added comparison checking isanagram

--- ValidAnagram/isAnagram.py
+++ ValidAnagram/isAnagram.py
@@ -6,8 +6,13 @@ class Solution(object):
         :rtype: bool
         """
         if len(s) != len(t):
-            return false
+            return False
         #use hashmap? trying to compare if every letter in string s exists in t
+        #map every letter to every other letter?
 
-        for x in enumerate(s):
-            print(x)
+        for i, x in enumerate(s):
+            if x not in t[i]:
+                return False
+
+        #should be at the end of both strings
+        return True
