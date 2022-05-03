69ceb4dd8c434ca705fc071cdb5178dad02558f2
Carlos Quintero <0xcquintero@gmail.com>
2022-05-03 15:23:21 +1000

    passed two tests is anagram using dicitonary

--- ValidAnagram/isAnagram.py
+++ ValidAnagram/isAnagram.py
@@ -5,14 +5,45 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+
         if len(s) != len(t):
             return False
-        #use hashmap? trying to compare if every letter in string s exists in t
-        #map every letter to every other letter?
-
+        #the below works in every case where there are no repeated letters
+        """
         for x in s:
             if x not in t:
                 return False
-
+            
         #should be at the end of both strings
         return True
+        """
+
+        """
+        Notes from vid explanation:
+        
+        using the characters of s, we can create the string t
+        made up of exact same list of characters
+        
+        make hashmap of characters -> num of occurences?
+        """
+        #attempt:
+        smap = {}  # characters -> num of occurences
+        tmap = {}
+
+        for x, y in zip(s, t):
+            if x not in smap:
+                #add to map
+                smap[x] = 1
+            elif x in smap:
+                smap[x] += 1
+
+            if y not in tmap:
+                tmap[y] = 1
+            elif y in smap:
+                tmap[y] += 1
+
+        # both maps should now be populated with values
+        if smap == tmap:
+            return True
+        else:
+            return False
