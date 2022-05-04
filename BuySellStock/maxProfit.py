87690e5c5c9bc3f829e6151fb06bc6dce4e6c645
Carlos Quintero <0xcquintero@gmail.com>
2022-05-05 00:18:29 +1000

    maxProfit brainstorm

--- BuySellStock/maxProfit.py
+++ BuySellStock/maxProfit.py
@@ -4,3 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        """
+        Brainstorming:
+        lowest value must be before highest value
+        one solution would be to scan, find the lowest value and highest
+        if highest val is before lowest, discard and search for next highest
+        return that difference
+        """
