276d4879c638be671fefed2e246bed1e0fcb61b5
Carlos Quintero <0xcquintero@gmail.com>
2022-05-06 17:32:04 +1000

    maxProfit sliding windows2

--- BuySellStock/maxProfit.py
+++ BuySellStock/maxProfit.py
@@ -11,25 +11,19 @@ class Solution(object):
         if highest val is before lowest, discard and search for next highest
         return that difference
         """
-        
         buy = 0
         sell = 0
-        
-        bindex = 0
-        sindex = 0
-        
+
+        buyDex = 0
+        sellDex = 1
+
         for i, x in enumerate(prices):
             if x < buy:
                 buy = x
-                bindex = i
             if x > sell:
                 sell = x
-                sindex = i
-                
+            sellDex++
+            buyDex++
         #new approach, have two pointers adjacent to each other, if left < right, increment right only
         #otherwise increment both
         #when best is reched, return difference
-                
-
-            
-        
