fd18de8b595056a6de68c775903e7cf7aa1930b7
Carlos Quintero <0xcquintero@gmail.com>
2022-05-24 22:11:41 +1000

    maxsubarray no k attempt

--- MaximumSubarray/maxSubArray.py
+++ MaximumSubarray/maxSubArray.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        largest = 0
-        for x in nums:
-            print(largest, x, largest+x)
-            if largest + x >= largest:
-                largest += x
-            if abs(x) > largest/2:
-                largest = 0
-            #else:
-                #largest = 0
-
-        return largest
+        maxSum = nums[0]
+        curSum = 0
+        for n in nums:
+            curSum += n  # add current sum to each value
+            maxSum = max(curSum, maxSum)
+            curSum = max(0, curSum)
+        return maxSum
