7d3084bdcca5d10b5a620d0a323d1fb7da5450a2
Carlos Quintero <0xcquintero@gmail.com>
2022-06-30 17:21:01 +1000

    preorder added treeNode definition

--- notes/TreeTraversal/preorderTraversal.py
+++ notes/TreeTraversal/preorderTraversal.py
@@ -11,29 +11,33 @@
     1,2,4,5,3
 """
 
-from distutils.command.sdist import sdist
-from turtle import setundobuffer
-
-
+'''
+# Node Class:
+class Node:
+    def init(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
 class Solution:
     
     #helper function, check if node has children
-    def HasChildren(self, root):
+    def HasChildren(root):
         if root.left is not None and root.right is not None:
             return True
         return False
 
     def PreOrder(self, root):
-
         #create stack, initialise curr to root
         curr = root
         stack = []
 
         #iterate thru
         while True:
-
-            if curr is not None:
-                #
+            
+            if HasChildren(curr):
+                print(curr.data)
+                
 
             elif(stack):
                 #more stuff
