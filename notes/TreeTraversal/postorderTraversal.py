0df1ee3c883da5e019e27bb92784f6c4be3f2fc7
Carlos Quintero <0xcquintero@gmail.com>
2022-07-04 23:55:10 +1000

    postorder node def

--- notes/TreeTraversal/postorderTraversal.py
+++ notes/TreeTraversal/postorderTraversal.py
@@ -1,5 +1,13 @@
 #postorder is left, right, root
 
+'''
+# Node Class:
+class Node:
+    def init(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
 
 class Solution:
     def PostOrder(self, root):
