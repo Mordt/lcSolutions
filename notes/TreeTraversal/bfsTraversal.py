4fb2b475a23be1304391202a36a1bc718646cd4b
Carlos Quintero <0xcquintero@gmail.com>
2022-07-06 16:14:42 +1000

    levelorder online recursive

--- notes/TreeTraversal/bfsTraversal.py
+++ notes/TreeTraversal/bfsTraversal.py
@@ -41,7 +41,7 @@ class Solution:
         while True:
             curr = queue.pop(0)
             print(curr)
-            
+
             if curr.left is not None:
                 queue.append(curr.left)
             
@@ -69,3 +69,35 @@ class Solution:
             print(queue)
 
 """
+
+#recursive solution
+# Function to print all nodes of a given level from left to right
+
+
+def printLevel(root, level):
+
+    # base case
+    if root is None:
+        return False
+
+    if level == 1:
+        print(root.key, end=' ')
+
+        # return true if at least one node is present at a given level
+        return True
+
+    left = printLevel(root.left, level - 1)
+    right = printLevel(root.right, level - 1)
+
+    return left or right
+
+
+# Function to print level order traversal of a given binary tree
+def levelOrderTraversal(root):
+
+    # start from level 1 — till the height of the tree
+    level = 1
+
+    # run till printLevel() returns false
+    while printLevel(root, level):
+        level = level + 1
