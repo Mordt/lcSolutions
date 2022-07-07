351e1685694d72401d9c947902fd07bf5b41178c
Carlos Quintero <0xcquintero@gmail.com>
2022-07-07 13:44:53 +1000

    bstCousins added trackers

--- BSTCousins/isCousins.py
+++ BSTCousins/isCousins.py
@@ -21,10 +21,16 @@ class Solution(object):
         #keep track of xdepth, ydepth
         #find xparent, yparent
 
-        queue = deque()
+        curr = root
         xdepth, ydepth = 0
-        nodeMap = defaultdict()
-        queue.append((root, 0, 0))
+        queue = deque()
+        queue.append(curr)
+
+        parMap = {}#map of nodes->parents
+        parMap[curr] = None
+        parMap[curr.left] = curr
+        parMap[curr.right] = curr
+        
 
         while True:
 
