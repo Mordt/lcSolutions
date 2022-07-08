7094f5501067614348b21b9eae8f8e36bcc008a0
Carlos Quintero <0xcquintero@gmail.com>
2022-07-08 20:19:05 +1000

    bfs levels comments

--- notes/GeneralAlgos/bfsLevels.c
+++ notes/GeneralAlgos/bfsLevels.c
@@ -1,6 +1,14 @@
+/*
+We just need to know how many nodes the current level contains, 
+then we can deal with all the nodes in the same level, and increase the level by 1 after 
+we are done processing all the nodes on the current level.
+
+*/
+
 int level = 0;
 Queue<Node> queue = new LinkedList<>();
 queue.add(root);
+
 while(!queue.isEmpty()){
     int level_size = queue.size();
     while (level_size-- != 0) {
