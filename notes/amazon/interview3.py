9aec5fdee9dc82049f4c540130c6236ea495d743
Carlos Quintero <0xcquintero@gmail.com>
2022-07-13 18:23:11 -0500

    amazon onsite prep notes

---
+++ notes/amazon/interview3.py
@@ -0,0 +1,18 @@
+# interview notes for final round/onsite interviews with amazon
+# 
+
+onsite interview preparation
+  up to 5 1hr interviews.
+  will be asked 1 technical questoin in each interview slot (leetcode style question)
+  will also be asked 2 or 3 behavioural questions based on Leadership Principles.
+
+Technical Questions
+  technical questions will cover 3 main areas:
+    1. CS fundamentals, Data Structures and Algorithms, Object-Oriented Design
+    2. Coding
+    3. System Design
+  can be asked verbal questions, such as "given n sorted arrays, return a single sorted array"
+  MAKE SURE to ask clarifying questions and state your assumptions. have a clear picture of the 
+  problem before you start solving it.
+  this will apply to daily work as you always want to completely understand a problem before you begin solving it.
+
