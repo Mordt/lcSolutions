c9af1b015e954f38836203eb404c82939cbbe4e1
Carlos Quintero <0xcquintero@gmail.com>
2022-07-13 15:27:24 -0500

    set introduction

--- notes/sets.py
+++ notes/sets.py
@@ -1,3 +1,13 @@
-#notes taken on Sets, one of Pythons 4 default data structures.
+# notes taken on Sets, one of Pythons 4 default data structures.
 # the others are Lists, Tuples and Dictionaries (hashmaps)
+
+sets are collections which are unordered, unchangeable, and unindexed.
+
+  set items are unchangeable, but you can remove items and add new ones.
+
+set creation:
+
+set = {"one", "two", "three"}
+
+sets do not allow for duplicate items.
 
