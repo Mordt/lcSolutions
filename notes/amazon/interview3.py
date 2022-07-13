9329244f2fd90f6c3f4c8541d4e169296e7736c9
Carlos Quintero <0xcquintero@gmail.com>
2022-07-13 18:26:52 -0500

    amazon onsite prep notes technical questions

--- notes/amazon/interview3.py
+++ notes/amazon/interview3.py
@@ -16,3 +16,11 @@ Technical Questions
   problem before you start solving it.
   this will apply to daily work as you always want to completely understand a problem before you begin solving it.
 
+  once you understand the problem, keep an eye on the time and start iteratively solving the problem.
+  this means starting small and in an innefficient way, perhaps, but make it more efficient as you go along.
+  Break it down into smaller problems. Look at specific use cases and edge cases. 
+
+  Start with a simplified version of the problem then work your way up to the full scope!!
+
+  prepare commonly used algos such as sorting, binary searching and data structures such as HashMaps, Tries, and Linked Lists.
+
