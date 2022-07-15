7c3444715913faf4557355197e2167aa06644fb2
Carlos Quintero <0xcquintero@gmail.com>
2022-07-14 19:11:44 -0500

    tries introduction

--- notes/tries.py
+++ notes/tries.py
@@ -1,1 +1,8 @@
 notes on the Trie data structure
+
+Intro
+  tries are an efficient data retrieval data structure.
+  search complexities can be brought down to optimal time (key length) with tries.
+  
+  a well balanced BST will allow you to search for values in O(M*logn) time, where M is the length of the key.
+  in contrast, a trie can search for the key in O(M) time. The tradeoff comes in terms of space.
