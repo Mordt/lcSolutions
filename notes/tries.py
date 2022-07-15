529e887578baf08ffc86baafe30e64cbb0f50b85
Carlos Quintero <0xcquintero@gmail.com>
2022-07-14 19:18:58 -0500

    tries structure

--- notes/tries.py
+++ notes/tries.py
@@ -6,3 +6,12 @@ Intro
   
   a well balanced BST will allow you to search for values in O(M*logn) time, where M is the length of the key.
   in contrast, a trie can search for the key in O(M) time. The tradeoff comes in terms of space.
+
+structure
+  every trie node consists of a set of multiple branches alongside a boolean end of word key.
+  the key isendofword is used to distinguish the last node in a trie node.
+  an english alphabet node appears as follows
+
+  """
+
+  """
