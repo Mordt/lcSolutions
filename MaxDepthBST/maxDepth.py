aaea2dd0f7d6c1cb43bdc2f42ed637b27a2239f3
Carlos Quintero <0xcquintero@gmail.com>
2022-07-16 18:02:04 -0500

    progress on maxlevel

--- MaxDepthBST/maxDepth.py
+++ MaxDepthBST/maxDepth.py
@@ -4,10 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+#
 class Solution(object):
     def maxDepth(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
+        #classic level order traversal
+        #keep track of level as u go
         
+        level = 0
+        queue = []
+        queue.append(root)
+        
+        while(queue):
+            levelSize = len(queue)
+            while(levelSize != 0):
+                
+                curr = queue.pop(0)
