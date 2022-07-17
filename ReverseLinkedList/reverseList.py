9786bcc0ea8e66205268bfbac2646cc21b79934e
Carlos Quintero <0xcquintero@gmail.com>
2022-07-17 16:21:57 -0500

    problem file reverselist

---
+++ ReverseLinkedList/reverseList.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
+        if head == None:
+            return None
+        
+        curr = head
+        while curr.next is not None:
+            nextPtr = curr.next
+            
