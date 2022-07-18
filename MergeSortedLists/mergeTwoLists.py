22abed000d34194c48c0e7398bfdfebf7d882982
Carlos Quintero <0xcquintero@gmail.com>
2022-07-18 16:27:43 -0500

    first submission merge sorted lists

--- MergeSortedLists/mergeTwoLists.py
+++ MergeSortedLists/mergeTwoLists.py
@@ -15,7 +15,18 @@ class Solution(object):
         
         while list1 and list2:
             if list1.val < list2.val:
-                tail.next = list1.val
+                tail.next = list1
+                list1 = list1.next
             else:#list2.val < list1.val
-                tail.next = list2.val
-                
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+            
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+            
+        return dummy.next
+
+
