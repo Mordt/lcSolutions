dc17f8794a39dba0436ea2c8912e035061344c0d
Carlos Quintero <0xcquintero@gmail.com>
2022-07-19 11:45:25 -0500

    binary search prob file

---
+++ BinarySearch/search.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        middle = len(nums/2)
+        if target > nums[middle]:
+        
+        
+        return -1
+        
