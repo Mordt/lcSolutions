6d51d6be26a34f38d0f09ca6a06caf30170dff81
Carlos Quintero <0xcquintero@gmail.com>
2022-09-02 20:55:57 +1000

    last stone weight first attempt

--- LastStoneWeight/lastStoneWeight.py
+++ LastStoneWeight/lastStoneWeight.py
@@ -4,3 +4,20 @@ class Solution(object):
         :type stones: List[int]
         :rtype: int
         """
+        #heap/priority queue
+        #sort the list, smash from heaviest to lightest
+        
+        
+        while len(stones) > 1:
+            stones.sort()
+            y = stones.pop()
+            x = stones.pop()
+            
+            if x == y:
+                continue
+            else:
+                y -= x
+                stones.insert(0, y)
+        
+        return stones[0]
+    
