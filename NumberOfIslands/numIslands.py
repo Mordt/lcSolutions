6c6b934089cad2c9b2aa71a9651afbd97c2309b2
Carlos Quintero <0xcquintero@gmail.com>
2022-09-03 18:19:25 +1000

    added logic for a graph bfs traversal

--- NumberOfIslands/numIslands.py
+++ NumberOfIslands/numIslands.py
@@ -4,4 +4,34 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
+        #brute force solution
+        #construct a graph from the grid
+        #bfs graph traversal
         
+    graph = {
+      'A' : ['B','C'],
+      'B' : ['D', 'E'],
+      'C' : ['F'],
+      'D' : [],
+      'E' : ['F'],
+      'F' : []
+    }
+
+    visited = [] # List to keep track of visited nodes.
+    queue = []     #Initialize a queue
+
+    def bfs(visited, graph, node):
+      visited.append(node)
+      queue.append(node)
+
+      while queue:
+        s = queue.pop(0) 
+        print (s, end = " ") 
+
+        for neighbour in graph[s]:
+          if neighbour not in visited:
+            visited.append(neighbour)
+            queue.append(neighbour)
+
+
+
