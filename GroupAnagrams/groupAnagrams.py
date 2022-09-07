d3afd4499bf699f00a3cb24314f0d9d91a4039a7
Carlos Quintero <0xcquintero@gmail.com>
2022-09-07 18:12:20 +1000

    group anagrams neetcode approach 2

--- GroupAnagrams/groupAnagrams.py
+++ GroupAnagrams/groupAnagrams.py
@@ -8,10 +8,13 @@ class Solution(object):
         #hashmap mapping letters to words
         #so count e, count a, count t, mapped to the words that have them
 
-        result = {}  # char count of each string -> list of anagrams
+        # char count of each string -> list of anagrams
+        result = defaultdict(list)
 
         for s in strs:
             count = [0] * 26
 
             for c in s:
-                count[ord(c) - ord("a")]
+                count[ord(c) - ord("a")] += 1
+
+            result[tuple(count)].append(s)
