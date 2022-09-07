c8942b4e70140e6c4ca02e523a438f28a611d6d2
Carlos Quintero <0xcquintero@gmail.com>
2022-09-07 18:10:09 +1000

    group anagrams neetcode approach

--- GroupAnagrams/groupAnagrams.py
+++ GroupAnagrams/groupAnagrams.py
@@ -1,28 +1,17 @@
 class Solution(object):
-    def isAnagram(s, t):
-        if len(s) != len(t):
-            return False
-
-        countS, countT = {}, {}
-
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        for c in countS:
-            if countS[c] != countT.get(c, 0):
-                return False
-        return True
-
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        #note: sort and compare might not be too bad
+        #new idea:
+        #hashmap mapping letters to words
+        #so count e, count a, count t, mapped to the words that have them
 
-        #strategy is to compare whether anagrams are valid, if so, group
-        #by calling is anagram
-        for x in strs:
-            for y in strs:
-                #for x, i in enumerate(strs):
-                #    print(x, i)
+        result = {}  # char count of each string -> list of anagrams
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")]
