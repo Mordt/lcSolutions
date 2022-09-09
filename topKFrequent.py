d99854901e3ab8386fe9e32426296b9626176b8d
Carlos Quintero <0xcquintero@gmail.com>
2022-09-09 19:15:58 +1000

    topkfreq added map

--- topKFrequent.py
+++ topKFrequent.py
@@ -9,5 +9,13 @@ class Solution(object):
         #iterate through list, count every instance and add to map
         #at the end, grab the top k
         
+        freq = {}
+        
         for n in nums:
-            
+            if n not in freq:
+                freq[n] = 1
+            else:
+                freq[n] += 1
+                
+        print(freq)
+
