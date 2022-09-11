bb795c275ac6362b678a7747dc940eab9382cd38
Carlos Quintero <0xcquintero@gmail.com>
2022-09-11 21:34:24 +1000

    added previous attempt

--- ProductOfArray/productExceptSelf.py
+++ ProductOfArray/productExceptSelf.py
@@ -30,3 +30,28 @@ class Solution(object):
         print(prefix)
         print(postfix)
 
+#my earlier solution:
+
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        #idea: go through nums,
+        #create map of self -> product without self
+
+        product = 1
+        for n in nums:
+            product = product * n
+
+        prodSans = {}
+        for n in nums:
+            prodSans[n] = product/n
+
+        result = []
+        for key in prodSans:
+            result.append(prodSans[key])
+
+        return result
