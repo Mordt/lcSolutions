00d4aedb1cabe8aefdd4fe55a1ef7c7a84d3dcff
Carlos Quintero <0xcquintero@gmail.com>
2022-09-30 19:34:59 +1000

    iterating thru nums

---
+++ TwoSum/twoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //map each number to its index
+        //if index - targen exists in hashmap, return indices
+        //otherwise return nothing
+        
+        //making hashmap
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.size(); i++){
+            System.out.println(nums[i]);
+            
+        }
+        
+    }
+}
+
