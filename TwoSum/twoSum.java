9490f83a4aae56ae2c2f3426141d351f4386c4b7
Carlos Quintero <0xcquintero@gmail.com>
2022-09-30 19:36:09 +1000

    iterating thru nums syntax

--- TwoSum/twoSum.java
+++ TwoSum/twoSum.java
@@ -6,11 +6,10 @@ class Solution {
         
         //making hashmap
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i = 0; i < nums.size(); i++){
+        for(int i = 0; i < nums.length; i++){
             System.out.println(nums[i]);
             
         }
         
     }
 }
-
