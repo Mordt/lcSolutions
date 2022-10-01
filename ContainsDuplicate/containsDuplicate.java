b9106c6d976f64b11dcf6a7dd611895bb7f1974e
Carlos Quintero <0xcquintero@gmail.com>
2022-10-01 11:56:36 +1000

    containsDuplicate strategy comments

--- ContainsDuplicate/containsDuplicate.java
+++ ContainsDuplicate/containsDuplicate.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        //iterate thru, add to map
+        //if not in map, continue
+        //if in map, return true as contains duplicate
+        //else return false
         
     }
 }
