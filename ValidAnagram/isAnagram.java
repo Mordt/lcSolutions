6cf819e181bd7cebbb55a8f309193b2598c3227d
Carlos Quintero <0xcquintero@gmail.com>
2022-10-01 12:31:52 +1000

    javaValidAnagram debugging

--- ValidAnagram/isAnagram.java
+++ ValidAnagram/isAnagram.java
@@ -1,8 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         //approach is to sort both, if the same, return true
-        String sPrime = Arrays.sort(s);
-        String tPrime = Arrays.sort(t);
+        String[] sArray = new String[]{s};
+        String[] tArray = new String[]{t};
+        
+        String[] sPrime = Arrays.sort(sArray);
+        String[] tPrime = Arrays.sort(tArray);
         
         if(sPrime == tPrime){
             return true;
