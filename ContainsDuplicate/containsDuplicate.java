45b9d9fa750cb0be552bba2da5a58af42ac0d187
Carlos Quintero <0xcquintero@gmail.com>
2022-10-01 12:07:59 +1000

    containsDuplicate set, loop etc

--- ContainsDuplicate/containsDuplicate.java
+++ ContainsDuplicate/containsDuplicate.java
@@ -4,6 +4,16 @@ class Solution {
         //if not in map, continue
         //if in map, return true as contains duplicate
         //else return false
+        Set<Integer> set = new HashSet<Integer>();
+        boolean result = false;
         
+        for(int i = 0; i < nums.length; i++){
+            if(map.contains(nums[i])){
+                result = true;
+            } else {
+                map.add(nums[i]);
+            }
+        }
+        return result;
     }
 }
