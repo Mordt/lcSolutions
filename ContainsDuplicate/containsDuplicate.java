f8092f74bbd53a260d9415c0f11438c49dbcb2ef
Carlos Quintero <0xcquintero@gmail.com>
2022-10-01 12:08:32 +1000

    containsDuplicate debugging

--- ContainsDuplicate/containsDuplicate.java
+++ ContainsDuplicate/containsDuplicate.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        //iterate thru, add to map
-        //if not in map, continue
-        //if in map, return true as contains duplicate
+        //iterate thru, add to set
+        //if not in set, continue
+        //if in set, return true as contains duplicate
         //else return false
         Set<Integer> set = new HashSet<Integer>();
         boolean result = false;
         
         for(int i = 0; i < nums.length; i++){
-            if(map.contains(nums[i])){
+            if(set.contains(nums[i])){
                 result = true;
             } else {
-                map.add(nums[i]);
+                set.add(nums[i]);
             }
         }
         return result;
