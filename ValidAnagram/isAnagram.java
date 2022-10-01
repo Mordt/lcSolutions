de1a89156d55e9a89cd11f6dca742bee2f8b6abd
Carlos Quintero <0xcquintero@gmail.com>
2022-10-01 12:50:47 +1000

    javaValidAnagram first submission

--- ValidAnagram/isAnagram.java
+++ ValidAnagram/isAnagram.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //approach is to sort both, if the same, return true
+  
         char[] sArray = s.toCharArray();
         char[] tArray = t.toCharArray();
-        
+
         Arrays.sort(sArray);
         Arrays.sort(tArray);
         
-        if(sArray == tArray){
-            return true;
-        } else {
-            return false;
+        boolean result = true;
+        for(int i = 0; i < sArray.length; i++){
+            if(sArray[i] != tArray[i]){
+                result = false;
+            }
         }
+        return result;
+
     }
 }
