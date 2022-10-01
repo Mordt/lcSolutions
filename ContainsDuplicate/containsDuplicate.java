53869299af74088625c746717d220b0feb4ed3c2
Carlos Quintero <0xcquintero@gmail.com>
2022-10-01 12:10:50 +1000

    containsDuplicate final comments

--- ContainsDuplicate/containsDuplicate.java
+++ ContainsDuplicate/containsDuplicate.java
@@ -1,9 +1,16 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        //iterate thru, add to set
-        //if not in set, continue
-        //if in set, return true as contains duplicate
-        //else return false
+        /*
+            iterate thru, add to set
+            if not in set, continue
+            if in set, return true as contains duplicate
+            else return false
+        
+            Reason for using set is that we can do set.contains
+            in constant time. not a hashmap because we dont have
+            key->value pairs.
+        */
+
         Set<Integer> set = new HashSet<Integer>();
         boolean result = false;
         
