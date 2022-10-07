8c96679fca69884e974e57dec62bccb7b1a697a0
Carlos Quintero <0xcquintero@gmail.com>
2022-10-07 19:06:01 +1100

    overall program structure

--- FizzBuzz/fizzBuzz.java
+++ FizzBuzz/fizzBuzz.java
@@ -1,5 +1,16 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         
+        List<String> result = new List<String>();
+        if(n % 3 == 0 && n% 5 == 0){
+            //do stuff
+        } else if(n % 3 == 0){
+            //sf
+        } else if(n % 5 == 0){
+            //
+        } else {
+            result = n.string();
+        }
+        return result;
     }
 }
